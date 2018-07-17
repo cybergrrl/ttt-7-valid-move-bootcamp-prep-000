e6454ebbbafd3e490e3f676a164c8db98fbf33c5
scout b <scout.burghardt@gmail.com>
2018-07-17 18:56:04 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,15 @@
 # code your #valid_move? method here
-
+def valid_move?(array, position)
+  if position.between?(0, 8)
+    return true
+  end
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(array, position)
   if (array[position] == "X" || array[position] == "Y")
-    return false
-  else return true
+    return true
+  else return false
   end
 end
