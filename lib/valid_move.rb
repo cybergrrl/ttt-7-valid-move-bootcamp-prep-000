1e722262dad8e46fa28d6a3a816d18667f18f8b1
scout b <scout.burghardt@gmail.com>
2018-07-17 18:54:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,5 @@
 # code your #valid_move? method here
-def valid_move?(position)
-  if position.between?(0, 8)
-    return true
-  end
-end
+
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
